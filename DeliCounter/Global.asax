99cb22094e7ed92565788a9d6aa8992d083fdd97
thatknitter <kblewis.flt@gmail.com>
2015-03-02 10:05:42 -0600

    initial commit

---
+++ DeliCounter/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DeliCounter.MvcApplication" Language="C#" %>
---
+++ DeliCounter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DeliCounter.Startup))]
+namespace DeliCounter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
